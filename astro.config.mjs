4247022c2b2a311672eb427e68c2a3f13be3c3f7
Lúcio Andrade <lucioandradejr@gmail.com>
2023-11-27 15:37:18 -0400

    initial commit ✨

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    server: {
+      watch: {
+        ignored: ['**/target/**']
+      }
+    }
+  },
+  integrations: [tailwind()]
+});
