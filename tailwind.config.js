15815274328db5f1758fcd7aedb9dde3be4cf27b
Lúcio Andrade <lucioandradejr@gmail.com>
2023-11-27 16:21:27 -0400

    feat: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          foreground: 'rgba(var(--foreground), <alpha-value>)',
+          background: 'rgb(var(--background), <alpha-value>)',
+          muted: {
+            DEFAULT: 'rgba(var(--muted), <alpha-value>)',
+            gray: 'rgba(var(--muted-gray), <alpha-value>)',
+            foregorund: 'rgba(var(--muted-foreground), <alpha-value>)',
+          },
+          gray: 'rgba(var(--gray), <alpha-value>)',
+        }
+    },
   },
   plugins: [],
 }
